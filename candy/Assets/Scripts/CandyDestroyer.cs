96900cc51a9cb8d8c76b5990871e330113a0f8e9
kazuya0105 <55573361+kazuya0105@users.noreply.github.com>
2020-01-25 14:37:49 +0900

    add-candy
    お願いします。

---
+++ candy/Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    public CandyHolder candyHolder;
+    public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider collider){
+        if(collider.gameObject.tag=="Candy"){
+            candyHolder.AddCandy(reward);
+            Destroy(collider.gameObject);
+            if(effectPrefab!=null){
+                Instantiate(
+                    effectPrefab,
+                    collider.transform.position,
+                    Quaternion.Euler(effectRotation)
+                );
+            }
+        }
+    }
+}
