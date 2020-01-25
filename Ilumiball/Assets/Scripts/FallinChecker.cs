3129537f4c00fab19422a4a3eb47cbba2fa596cd
kazuya0105 <55573361+kazuya0105@users.noreply.github.com>
2020-01-25 15:53:44 +0900

    dev-v02
    illumiballの追加

---
+++ Ilumiball/Assets/Scripts/FallinChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallinChecker : MonoBehaviour
+{
+    public Hole red;
+    public Hole blue;
+    public Hole green;
+    void OnGUI(){
+        string label="";
+        if(red.IsFallIn()&&blue.IsFallIn()&&green.IsFallIn()){
+            label="Fall in hole";
+        }
+        GUI.Label(new Rect(0,0,100,30),label);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
